d3c87bf9366abb6283553f7d870a2e0d9b319d4f
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2020-07-11 19:28:19 +0200

    refactoring

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-	input: 'src/be.jeroenvdb.googlemeet.sdPlugin/plugin.ts',
+	input: 'src/be.jeroenvdb.googlemeet.sdPlugin/index.ts',
 	output: {
 		file: 'build/be.jeroenvdb.googlemeet.sdPlugin/bundle.js',
 		format: 'iife'
